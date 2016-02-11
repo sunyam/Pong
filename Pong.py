271da598be849fd5e02a03b0ae1141afbbfd5c29
sunyam <sunyambagga@gmail.com>
2016-02-11 21:10:47 +0530

    adding ball spawner and new_game() functions

--- Pong.py
+++ Pong.py
@@ -20,3 +20,22 @@ paddle1_vel = 0
 paddle2_vel = 0
 score1 = 0
 score2 = 0
+
+# initialize ball_pos and ball_vel for new bal in middle of table
+# if direction is RIGHT, the ball's velocity is upper right, else upper left
+def spawn_ball(direction):
+    global ball_pos, ball_vel # these are vectors stored as lists
+    ball_pos = [WIDTH/2, HEIGHT/2]
+    if direction == "RIGHT":
+        ball_vel = [random.randrange(2,4), -random.randrange(1,3)]
+    elif direction == "LEFT":
+        ball_vel = [-random.randrange(2,4), -random.randrange(1,3)]
+        
+# define event handlers
+def new_game():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
+    global score1, score2  # these are ints
+    score1 = 0
+    score2 = 0
+    spawn_ball("LEFT")
+    
